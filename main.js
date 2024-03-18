6ffbad30d4470504f9e8aac9945a260d011fbfda
twingoose <140074426+twingoose@users.noreply.github.com>
2024-03-18 09:58:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+Math.floor(Math.random() * 10) + 1;
+Syntax
+
+Math.floor(){
+array_1=[‘pen’,’paper’,’book’,’bottle’]
+
+random_no = Math.floor((Math.random()*array_1.length)+1)
+
+Element_of_array = array_1[random_no]
+}
